bf80d15bd07ef4b171efd96a18a42d55b4c88603
Khayal <khayalmans994@gmail.com>
2023-10-12 05:23:01 +0400

    ve bitdi

--- apps.js
+++ apps.js
@@ -5,3 +5,27 @@ function openPopup() {
 function closePopup() { 
     popup.classList.remove("open-popup") 
 }
+function convertCurrency() {
+    var amount = document.getElementById("amount").value;
+    var currency = document.getElementById("currency").value;
+    var result = 0;
+
+    if(currency === "usd") {
+        result = amount / 1.7; 
+    } else if(currency === "eur") {
+        result = amount / 1.80;
+    } else if(currency === "gbp") {
+        result = amount / 0.61; 
+    }
+    switch (currency) {
+        case "usd":
+            document.getElementById("result").innerHTML = (amount + "azn = " + result + "$")
+            break;
+        case "eur":
+            document.getElementById("result").innerHTML = (amount + "azn = " + result + "€")
+            break;
+            case "gbp":
+                document.getElementById("result").innerHTML = (amount + "azn = " + result + "₺")
+                break;
+    }
+}
